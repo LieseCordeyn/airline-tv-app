dc4775babe7c47888980e8426d6b7b7c9ea439e7
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-04-01 13:16:09 +0200

    last changes day 5 :airplane:

--- airplane-tv/src/components/Header/header.tsx
+++ airplane-tv/src/components/Header/header.tsx
@@ -5,7 +5,10 @@ import airplane from '../../images/airplane.png'
 
 function header(){
    return( <header>
-       <img src={airplane} alt="airplane" id="airplane"/>
+          <img src={airplane} alt="airplane" id="airplane"/>
+          <img src={airplane} alt="airplane" id="airplane2"/>
+          <img src={airplane} alt="airplane" id="airplane3"/>
+
           <p id="homeScreen">airplane-tv</p>
       </header>)
 }
